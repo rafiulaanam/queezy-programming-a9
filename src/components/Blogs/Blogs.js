6bff7762e12489d5b4272c4d4825b9380e65f318
rafiulaanam <rafiulaanam@gmail.com>
2022-10-11 23:03:37 +0600

    update blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -6,7 +6,26 @@ const Blogs = () => {
         <div>
            <h2 className='text-center'>Quiz Blog</h2>
 
-          
+           <Accordion defaultActiveKey={['0']} alwaysOpen>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>What is the purpose of React Router</Accordion.Header>
+        <Accordion.Body>
+        React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>How does context api work</Accordion.Header>
+        <Accordion.Body>
+        The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>What is useRef hook in React</Accordion.Header>
+        <Accordion.Body>
+        The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
         </div>
     );
 };
